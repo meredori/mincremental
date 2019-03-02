4256852bf7b18326a300199134436d474e431757
Meredori <durzan@gmail.com>
2019-03-02 13:36:09 +1000

    testing CI with build task

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './scss/main.scss';
+
+ReactDOM.render(<h1>Hello World Test</h1>,document.getElementById("main"));
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+const extractText = new ExtractTextPlugin({
+    filename : 'style.css'
+});
+
+module.exports = {
+  plugins: [htmlPlugin,extractText],
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+            {
+              fallback: 'style-loader',
+              use: ['css-loader', 'sass-loader']
+            })      
+        }
+    ]
+  }
+};
