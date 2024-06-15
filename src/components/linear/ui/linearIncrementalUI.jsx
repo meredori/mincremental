4bca1eaeac81eb2085ffa624fb9639434b5d016b
Mitchell lett <durzan@gmail.com>
2024-06-15 17:55:47 +1000

    fix same issue in linear jsx

--- src/components/linear/ui/linearIncrementalUI.jsx
+++ src/components/linear/ui/linearIncrementalUI.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Scoreboard from "../../common/scoreboard/scoreboard.jsx";
-import Incrementer from "../../common/incrementer/Incrementer.jsx";
+import Incrementer from "../../common/incrementer/incrementer.jsx";
 
 class LinearIncrementalUI extends React.Component {
     constructor(props){
